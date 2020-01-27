9ed137a86555efdc44398a860c5b73cad330a70a
Tan-Junda <Junda.Tan@uvm.edu>
2020-01-27 16:07:16 -0500

    add compile instructions

--- OpSqliteDB.java
+++ OpSqliteDB.java
@@ -24,27 +24,4 @@ public class OpSqliteDB {
             e.printStackTrace();
         } 
     }
-    
-
-/*
-    public static void func1(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        Statement statement1 = connection.createStatement();
-        statement.setQueryTimeout(30); // set timeout to 30 sec.
-        // 执行查询语句
-        ResultSet rs = statement.executeQuery("select * from table_name1");
-        while (rs.next()) {
-            String col1 = rs.getString("col1_name");
-            String col2 = rs.getString("col2_name");
-            System.out.println("col1 = " + col1 + "  col2 = " + col2);
-            
-           //System.out.println(location);
-            // 执行插入语句操作
-            statement1.executeUpdate("insert into table_name2(col2) values('" + col2_value + "')");
-            // 执行更新语句
-            statement1.executeUpdate("update table_name2 set 字段名1=" +  字段值1 + " where 字段名2='" +  字段值2 + "'");
-        }
-    }
-   */
-   
-}
+}    
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CS205-WarmUp-ML-RB-SN-JT
+
+javac -classpath ".:sqlite-jdbc-3.30.1.jar:" OpSqliteDB.java
+
+java -classpath ".:sqlite-jdbc-3.30.1.jar:" OpSqliteDB
