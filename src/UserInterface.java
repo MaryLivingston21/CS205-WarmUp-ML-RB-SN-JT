ce824bcfe5f79ca7437bce75e39de726494dc72f
marylivingston21 <mliving1@uvm.edu>
2020-01-29 16:16:13 -0500

    coding userInterface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class UserInterface {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Welcome to our program! \nEnter your name:  ");
+        String name = scanner.next();
+        System.out.println("Welcome " + name);
+
+    }
 }
