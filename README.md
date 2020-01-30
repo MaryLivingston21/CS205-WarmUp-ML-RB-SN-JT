bbb2967ade33eacc14de5a9beb3ef04b5d1a2698
Tan-Junda <Junda.Tan@uvm.edu>
2020-01-29 21:39:05 -0500

    use python for import csv data

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # CS205-WarmUp-ML-RB-SN-JT
 
+Compile and Run in Linux terminal:
 javac -classpath ".:sqlite-jdbc-3.30.1.jar:" OpSqliteDB.java
-
 java -classpath ".:sqlite-jdbc-3.30.1.jar:" OpSqliteDB
+
+#1
+include sqlite-jdbc-3.30.1.jar file in classpath or dependency
+
+#2
+install python3-pandas library
+use load.py to load data from csv files.
+load.py is called in the create_table function.
+
+windows: D:\Python36\Scripts\pip install pandas
--- src/OpSqliteDB.java
+++ src/OpSqliteDB.java
@@ -6,19 +6,21 @@ public class OpSqliteDB
     
    public static void main(String args[]) 
    {
-        Boolean load = false;
+        Boolean load = true;
         // User prompt
-        
-        
+                        
+        // create Table
+        if (load)
+        {
+           createTable();    
+        }   
+
         // Make Conection 
         Connection c = null;
         c = connect(c);
         
-        //create Table
-        if (load)
-        {
-           createTable(c);    
-        }   
+        // Query from table
+        // query(c);
 
     }
     
@@ -28,7 +30,7 @@ public class OpSqliteDB
         try 
         {   
            Class.forName("org.sqlite.JDBC");
-           c=DriverManager.getConnection("jdbc:sqlite:Test.db");                                    
+           c=DriverManager.getConnection("jdbc:sqlite:Film.db");                                    
                    
         } catch(Exception e) {
         
@@ -37,52 +39,40 @@ public class OpSqliteDB
         return c;
     }
     
-    public static void createTable(Connection c) 
+    public static void createTable() 
     {
         try 
-        {               
-            Statement statement = c.createStatement(); 
-
-            // create tables
-            statement.executeUpdate("create table directors( " +
-               "show_id	integer, " +
-               "type text, " +	
-               "title primary key," +
-               "director, " +	
-               "cast	text, " +
-               "country	text, " +
-               "release_year integer, " +
-               "rating text, " +
-               "duration text); ");
-               
-            statement.executeUpdate("create table titles( " +
-               "show_id	integer, " +
-               "type text, " +	
-               "title primary key, " +
-               "director text, " +	
-               "date_added text, " +	
-               "duration text, " +	
-               "description text ); ");
-            
-            // TODO insert tuples from csv to tabless
-            statement.executeUpdate("insert into directors values(81155784, " +	
-            "'Movie',	'Watchman',	'A. L. Vijay',"	+
-            "'G.V. Prakash Kumar, Samyuktha Hegde, Suman, Raj Arjun, Yogi Babu, Munishkanth'," +
-            "'India',	2019,	'TV-14',	'93 min');");
-            
-            
-            // query from tables  
-            
-                                    
-            
-        } catch (SQLException e) {
-        
-            System.err.println(e.getMessage());
-        
-        } catch(Exception e) {
-        
-            e.printStackTrace();
+        {                 
+           Runtime rt = Runtime.getRuntime();
+           Process pr = rt.exec("python load.py");               
+           BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));                
+           input.close();                                                                                                       
+                                   
+        } catch(Exception e) {        
+           e.printStackTrace();
         } 
     
+    }
+    
+    public static void query(Connection c) 
+    {
+       try
+       {       
+           Statement statement = c.createStatement(); 
+           ResultSet rs = statement.executeQuery("select * from directors limit 10");
+           while (rs.next()) {
+              String col1 = rs.getString("title");
+              System.out.println("title = " + col1);
+           }
+        
+       } catch (SQLException e) {
+           
+           System.err.println(e.getMessage());
+           
+       } catch(Exception e) {
+           
+           e.printStackTrace();
+           
+       } 
     }
 } 
