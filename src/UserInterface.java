b4d96471e3089fddff7e3d551ac7900bb5dc9287
marylivingston21 <mliving1@uvm.edu>
2020-01-31 15:41:41 -0500

    coding userInterface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,5 +8,25 @@ public class UserInterface {
         String name = scanner.next();
         System.out.println("Welcome " + name);
 
+        char choice = 'h';
+        while (choice != 'e') {
+            System.out.print("Menu: help (h) load data (l) get data (g) exit (e)");
+            choice = scanner.next().charAt(0);
+            switch (choice) {
+                case 'h':
+                    System.out.println("help instructions");
+                    break;
+                case 'l':
+                    System.out.println("call load data function here");
+                    break;
+                case 'e':
+                    System.out.println("exiting program");
+                    break;
+                case 'g':
+                    System.out.println("get data");
+                    break;
+            }
+        }
+
     }
 }
