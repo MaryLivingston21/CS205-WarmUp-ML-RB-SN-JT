c10e4e7a812c468502276d1da43522f5d4aa9790
marylivingston21 <mliving1@uvm.edu>
2020-01-31 15:44:53 -0500

    add errorChecking to userInterface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -9,6 +9,7 @@ public class UserInterface {
         System.out.println("Welcome " + name);
 
         char choice = 'h';
+        boolean loadData = false;
         while (choice != 'e') {
             System.out.print("Menu: help (h) load data (l) get data (g) exit (e)");
             choice = scanner.next().charAt(0);
@@ -17,13 +18,21 @@ public class UserInterface {
                     System.out.println("help instructions");
                     break;
                 case 'l':
-                    System.out.println("call load data function here");
+                    if (!loadData){
+                        System.out.println("call load data function here");
+                    } else {
+                        System.out.println("data is already loaded");
+                    }
                     break;
                 case 'e':
                     System.out.println("exiting program");
                     break;
                 case 'g':
-                    System.out.println("get data");
+                    if (loadData) {
+                        System.out.println("get data");
+                    } else {
+                        System.out.println("please load the data");
+                    }
                     break;
                 default:
                     System.out.println("please pick valid option");
