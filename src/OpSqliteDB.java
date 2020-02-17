30f57a79137420f0247b8c68672ae18365d02a60
Tan-Junda <Junda.Tan@uvm.edu>
2020-02-17 15:13:29 -0500

    make 5 parameters query function

--- src/OpSqliteDB.java
+++ src/OpSqliteDB.java
@@ -41,7 +41,59 @@ public class OpSqliteDB
         } 
     
     }
-    
+
+
+    public static void query(String output_t, String type1, String input1, String type2, String input2) {
+        String result = "";
+        try{
+            String col1 = "";
+            Statement statement = c.createStatement();
+            String table = "directors"; // default table
+            getTableName(table);
+//  select titles.rating from directors, titles where directors.director = titles.director and directors.country = 'India' and directors.director = 'A. L. Vijay' ;
+            String query = "select " + table + "."+ output_t + " from " + "directors, titles" + "where d.netflix_titles = t.titles and " + type1 +
+                                " = \"" + input1  + " and " + type2 + " = \"" + input2 + "\";" ;
+            // System.out.println(query); testing
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                col1 = rs.getString(output_t);
+                System.out.println(output_t + " " + col1);
+            }
+            result = output_t + "\t " + col1;
+
+
+        } catch (SQLException e) {
+
+            System.err.println(e.getMessage());
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+
+    private static String getTableName(String output_t) {
+        String v2 = "directors"; // default table
+        switch (output_t) {
+            // 2 cases for titles only
+            case "date_added": v2 = "titles"; break;
+            case "description": v2 = "titles"; break;
+            // 3 cases for directors only
+            case "actor": v2 = "directors"; break;
+            case "country": v2 = "directors"; break;
+            case "release_year": v2 = "directors"; break;
+            // Shared Cases
+            //         case "rating": v2 = "directors"; break;
+//            case "duration": v2 = "directors"; break;
+//            case "title": v2 = "directors"; break;
+            case "director": v2 = "directors"; break;
+
+        }
+        return v2;
+    }
+
     // TODO modify query function takes parameters as three parses in
     // TODO Make quries    
     // 1st be attribute, hwta user whats(output)
@@ -54,24 +106,9 @@ public class OpSqliteDB
        try
        {
            String col1 = "";
-           Statement statement = c.createStatement(); 
-           String v2 = "directors"; // default table
+           Statement statement = c.createStatement();
            
-           switch (output_t) {
-           // 2 cases for titles only
-           case "date_added": v2 = "titles"; break;
-           case "description": v2 = "titles"; break;
-           // 3 cases for directors only
-           case "actor": v2 = "directors"; break;
-           case "country": v2 = "directors"; break;
-           case "release_year": v2 = "directors"; break;
-           // Shared Cases
-   //         case "rating": v2 = "directors"; break;
-//            case "duration": v2 = "directors"; break;
-//            case "title": v2 = "directors"; break;
-            case "director": v2 = "directors"; break;
-           
-           }
+           String v2 = getTableName(output_t);
            //Now for when their output_t is in both tables, lets try to do the query 
            //and if there's a error, to try te other table
            
